00008b46e5f5755ee0a18a72d1619e575e2bb51f
YumYum6 <76712719+YumYum6@users.noreply.github.com>
2020-12-29 22:48:43 -0800

    rm -rf /etc/netwhttps://github.com/YumYum6ork/if-up.d/ntpdate

---
+++ chroot.steps.part.1.sh
@@ -0,0 +1,22 @@
+cat /tmp/gparted.rescuezilla.check.sh /usr/sbin/gparted.copy > /usr/sbin/gparted
+rm /usr/sbin/gparted.copy
+ln -s /usr/bin/pcmanfm /usr/bin/nautilus
+rm /usr/bin/{rpcclient,smbcacls,smbclient,smbcquotas,smbget,smbspool,smbtar}
+rm /usr/share/icons/*/icon-theme.cache
+rm -rf /usr/share/doc
+rm -rf /usr/share/man
+rm -rf /etc/network/if-up.d/ntpdate cat /tmp/gparted.rescuezilla.check.sh /usr/sbin/gparted.copy > /usr/sbin/gparted
+rm /usr/sbin/gparted.copy
+ln -s /usr/bin/pcmanfm /usr/bin/nautilus
+rm /usr/bin/{rpcclient,smbcacls,smbclient,smbcquotas,smbget,smbspool,smbtar}
+rm /usr/share/icons/*/icon-theme.cache
+rm -rf /usr/share/doc
+rm -rf /usr/share/man
+rm -rf /etc/network/if-up.d/ntpdate cat /tmp/gparted.rescuezilla.check.sh /usr/sbin/gparted.copy > /usr/sbin/gparted
+rm /usr/sbin/gparted.copy
+ln -s /usr/bin/pcmanfm /usr/bin/nautilus
+rm /usr/bin/{rpcclient,smbcacls,smbclient,smbcquotas,smbget,smbspool,smbtar}
+rm /usr/share/icons/*/icon-theme.cache
+rm -rf /usr/share/doc
+rm -rf /usr/share/man
+rm -rf /etc/network/if-up.d/ntpdate rm -rf /etc/network/if-up.d/ntpdate rm -rf /etc/network/if-up.d/ntpdate rm -rf /etc/network/if-up.d/ntpdate rm -rf /etc/network/if-up.d/ntpdate rm -rf /etc/network/if-up.d/ntpdate rm -rf /etc/network/if-up.d/ntpdate rm -rf /etc/network/if-up.d/ntpdate rm -rf /etc/network/if-up.d/ntpdate rm -rf /etc/network/if-up.d/ntpdate rm -rf /etc/network/if-up.d/ntpdate 
